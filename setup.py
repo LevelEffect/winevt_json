26aec44dc4870d4b5610b54bbb8ea97cb35e464b
Rob Noeth <rob.noeth@leveleffect.com>
2018-09-04 12:36:48 -0500

    Added MIT licence text and filled out setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-import setuptools
+import setuptools, find_packages
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -7,15 +11,39 @@ setuptools.setup(
     name="winevt_json",
     version="0.0.1",
     author="Rob Noeth",
-    author_email="rob.noeth@gmail.com",
+    author_email="oss@leveleffect.com",
     description="A utility to convert windows system event logs into json objects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LevelEffect/winevt_json",
-    packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: infosec :: Forensics :: Threat Hunting"
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords='infosec forensics threathunting',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    
+    # Required Packages
+    install_requires=['lxml', 'python-evtx'],
+
+
+    # Console entry point
+    entry_points=
+    {
+        'console_scripts': 
+        [
+            'winevt_json=winevt_json:main'
+        ]
+    },
+
+    # URLs
+    project_urls = {
+        'Bug Reports':  'https://github.com/LevelEffect/winevt_json/issues',
+        'Source':       'https://github.com/LevelEffect/winevt_json/'
+    }
+
 )
